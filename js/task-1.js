a7e0be56c256f9b4d6ec417ca61c27cd32d001cd
Mr.Igor <iv_mirsoft@ukr.net>
2025-05-14 21:51:06 +0300

    hw-7

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,14 @@
-function makeTransaction(quantity, pricePerDroid) {
-	const totalPrice = quantity * pricePerDroid;
-	return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-}
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+const catArray = document.querySelector("ul#categories");
+
+if (catArray) {
+	const catNumber = catArray.children.length;
+	console.log(`Number of categories: ${catNumber}`);
+
+	for (const element of catArray.children) {
+		const catName = element.querySelector("h2").textContent;
+		const itemQty = element.querySelectorAll("li").length;
+		console.log(`Category: ${catName}`);
+		console.log(`Category: ${itemQty > 0 ? itemQty : 0}`);
+	}
+}
--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,52 @@
-function getShippingMessage(country, price, deliveryFee) {
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    alt: "Alpine Spring Meadows",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    alt: "Nature Landscape",
+  },
+  {
+    url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    alt: "Lighthouse Coast Sea",
+  }
+];
 
-	const totalPrice = price + deliveryFee;
-	return `Shipping to ${country} will cost ${totalPrice} credits`;
+const elGallery = document.querySelector("ul.gallery");
+
+console.log(elGallery);
+
+for (const element of images) {
+
+  if (!element.url || !element.alt) {
+    continue;
+  }
+
+  const newLi = document.createElement("li");
+  const newImage = document.createElement("img");
+
+  newLi.classList.add("taskList")
+
+  newImage.setAttribute("src", element.url);
+  newImage.setAttribute("alt", element.alt);
+  newImage.width = "450";
+
+  newImage.classList.add("listItemImage")
+
+  newLi.append(newImage);
+  elGallery.append(newLi);
+
 }
-
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
